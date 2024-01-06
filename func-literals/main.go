955363eaaead3257a674ed4702293bc0fcb1b672
Dinesh Lawate <100606811+dineshd30@users.noreply.github.com>
2024-01-06 11:34:05 +0000

    use enum

--- func-literals/main.go
+++ func-literals/main.go
@@ -2,34 +2,45 @@ package main
 
 import "fmt"
 
+// Enum type
+type Operation int
+
+// Enum values
+const (
+	Add Operation = iota
+	Sub
+	Mul
+)
+
 func main() {
 	fmt.Println("Demo : function literals")
-	add := getCalculator("add")
+	add := getCalculator(Add)
 	fmt.Printf("Adding %d + %d = %d\n", 2, 4, add(2, 4))
 }
 
+// Function type calculator
 type calculator func(int, int) int
 
-func getCalculator(cmd string) calculator {
-	var operation calculator
+func getCalculator(cmd Operation) calculator {
+	var cal calculator
 	switch cmd {
-	case "add":
-		operation = func(a int, b int) int {
+	case Add:
+		cal = func(a int, b int) int {
 			return a + b
 		}
 
-	case "sub":
-		operation = func(a int, b int) int {
+	case Sub:
+		cal = func(a int, b int) int {
 			return a - b
 		}
 
-	case "mul":
-		operation = func(a int, b int) int {
+	case Mul:
+		cal = func(a int, b int) int {
 			return a - b
 		}
 	}
 
 	return func(a int, b int) int {
-		return operation(a, b)
+		return cal(a, b)
 	}
 }
